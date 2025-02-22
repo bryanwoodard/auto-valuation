6580c767d4737dc6cc47f95c05be3770d7be5224
Bryan Woodard <bwoodard40@gmail.com>
2025-02-21 22:03:57 -0800

    Minor cleanup

--- public/javascripts/modules/Init.js
+++ public/javascripts/modules/Init.js
@@ -1,11 +1,5 @@
 export const init = function(){
     console.log("initiliazing...");
-    
-    
     document.getElementById("ticker").addEventListener("change", AVclass.Utils.getStatements);
-   
-    
     document.getElementById("submit").addEventListener("click", AVclass.Utils.process)
-    //console.log("attached submit handler");
-
 }
